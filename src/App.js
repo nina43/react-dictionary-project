0501cd8bc1e84f0718162110afb077066a798702
nina43 <161338284+nina43@users.noreply.github.com>
2024-10-09 12:44:11 +0100

    added a search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary</h1>
-      <h3>Search for a definition</h3>
+      <div className="container">
+        <h1 className="text-center">Dictionary</h1>
+        <h3 className="text-center">Search for a definition</h3>
+        <Dictionary />
+      </div>
+      <footer className="app-footer">Coded by Martyna Ziarek</footer>
     </div>
   );
 }
